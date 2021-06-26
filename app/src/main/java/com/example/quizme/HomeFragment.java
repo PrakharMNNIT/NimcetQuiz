8187282274205a99a1157327fdf397ba25be50e2
Prakhar Shekhar Parthasarthi <prakharsp1502077@gmail.com>
2021-06-26 16:52:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quizme/HomeFragment.java
@@ -0,0 +1,84 @@
+package com.example.quizme;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.quizme.databinding.FragmentHomeBinding;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    FragmentHomeBinding binding;
+    FirebaseFirestore database;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        database = FirebaseFirestore.getInstance();
+
+        final ArrayList<CategoryModel> categories = new ArrayList<>();
+
+        final CategoryAdapter adapter = new CategoryAdapter(getContext(), categories);
+
+        database.collection("categories")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        categories.clear(); // to clear old data simultaneously 
+                        for (DocumentSnapshot snapshot : value.getDocuments()) {
+                            CategoryModel model = snapshot.toObject(CategoryModel.class);
+                            model.setCategoryId(snapshot.getId());
+                            categories.add(model);
+                        }
+                        adapter.notifyDataSetChanged(); // to update UI
+                    }
+                });
+
+
+        binding.categoryList.setLayoutManager(new GridLayoutManager(getContext(),2));
+        binding.categoryList.setAdapter(adapter);
+
+        binding.spinwheel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), SpinnerActivity.class));
+            }
+        });
+
+
+        // Inflate the layout for this fragment
+        return binding.getRoot();
+    }
+}
